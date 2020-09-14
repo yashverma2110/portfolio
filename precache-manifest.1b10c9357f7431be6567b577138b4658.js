62fb8a4e07727fb836e0ed2430347b97a3ff80cb
yashverma2110 <yash.silver2110@gmail.com>
2020-09-15 00:13:29 +0530

    Updates

---
+++ precache-manifest.1b10c9357f7431be6567b577138b4658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096413aed61dd7c83d131b02dced099e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a892ba49c9a0cc525b4e",
+    "url": "/portfolio/static/css/main.1678d4eb.chunk.css"
+  },
+  {
+    "revision": "98c3bb52fc106fa5ce56",
+    "url": "/portfolio/static/js/2.73c0bac6.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/portfolio/static/js/2.73c0bac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a892ba49c9a0cc525b4e",
+    "url": "/portfolio/static/js/main.96854001.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
