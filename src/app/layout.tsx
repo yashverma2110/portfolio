bddf7c4bb461a0fb9766e12c9abc233f2b5de8ea
Yash Verma <yash.silver2110@gmail.com>
2024-11-05 04:11:02 +0530

    chore: remove google verification token

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,9 +52,6 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
-  verification: {
-    google: "your-google-verification-code",
-  },
 };
 
 export default function RootLayout({
