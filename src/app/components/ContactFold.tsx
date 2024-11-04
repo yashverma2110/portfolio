981be95eae20da79a2bdd342c201eddf5c25aacc
Yash Verma <yash.silver2110@gmail.com>
2024-11-05 00:13:02 +0530

    feat: updating contact section for mobile

--- src/app/components/ContactFold.tsx
+++ src/app/components/ContactFold.tsx
@@ -9,24 +9,24 @@ export default function ContactFold() {
       <h2 className="text-2xl font-bold text-neutral-500">Socials 💬</h2>
 
       <section className="w-full text-white flex flex-col gap-4 font-semibold">
-        <div className="flex items-center gap-2">
-          <FaArrowRight />
-          <h3>Let&apos;s Connect on LinkedIn</h3>
-          <Link href={CONTACT.linkedin} target="_blank" className="bg-slate-500 text-sm rounded p-2 w-fit flex items-center gap-2">
+        <div className="flex items-center text-sm md:text-base gap-2">
+          <FaArrowRight className="hidden md:block" />
+          <h3><span className="hidden md:inline">Let&apos;s Connect on </span>LinkedIn <span className="inline md:hidden">Profile</span> @</h3>
+          <Link href={CONTACT.linkedin} target="_blank" className="bg-slate-500 whitespace-nowrap text-sm rounded p-2 w-fit flex items-center gap-2">
             <FaLinkedin />
-            <h3>@yash-verma-80279b16b</h3>
+            <h3>yash-verma-80279b16b</h3>
           </Link>
         </div>
-        <div className="flex items-center gap-2">
-          <FaArrowRight />
-          <h3>Follow me on X</h3>
+        <div className="flex items-center text-sm md:text-base gap-2">
+          <FaArrowRight className="hidden md:block" />
+          <h3>Follow me on X @</h3>
           <Link href={CONTACT.twitter} target="_blank" className="bg-slate-500 text-sm rounded p-2 w-fit flex items-center gap-2">
             <FaTwitter />
-            <h3>@we_chat_tech</h3>
+            <h3>we_chat_tech</h3>
           </Link>
         </div>
-        <div className="flex items-center gap-2">
-          <FaArrowRight />
+        <div className="flex items-center text-sm md:text-base gap-2">
+          <FaArrowRight className="hidden md:block" />
           <h3>Email Me @</h3>
           <Link href={`mailto:${CONTACT.email}`} target="_blank" className="bg-slate-500 text-sm rounded p-2 w-fit flex items-center gap-2">
             <FaEnvelope />
